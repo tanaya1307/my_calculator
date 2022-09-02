f11d4ae33fc00ba7f3ce828ea26dccfb6a6a600f
Tanaya <placetocalltanaya@gmail.com>
2022-09-02 21:46:47 +0530

    initial commit

---
+++ src/OperationButton.js
@@ -0,0 +1,6 @@
+import {ACTIONS} from './App'
+
+
+export default function DigitButton({dispatch,operation}){
+    return <button onClick={()=>dispatch({type:ACTIONS.CHOOSE_OPERATION,payload:{operation}})}>{operation}</button>
+}
